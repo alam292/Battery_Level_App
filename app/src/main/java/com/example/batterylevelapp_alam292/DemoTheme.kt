37ef4af3f0d9651bf5a78159fec5bab965d9f42b
mdmat <mdmatloobgaya@gmail.com>
2025-07-09 00:16:05 +0530

    First commit

---
+++ app/src/main/java/com/example/batterylevelapp_alam292/DemoTheme.kt
@@ -0,0 +1,18 @@
+package com.example.batterylevelapp_alam292
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DemoTheme(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/batterylevelapp_alam292/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.batterylevelapp_alam292
+
+import android.content.Context
+import android.os.BatteryManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoTheme {
+                BatteryLevelScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun BatteryLevelScreen() {
+    val context = LocalContext.current
+    val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+    val batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Battery Level: $batteryLevel%",
+            fontSize = 30.sp
+        )
+    }
+}
